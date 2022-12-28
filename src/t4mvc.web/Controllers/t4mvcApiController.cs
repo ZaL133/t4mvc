0012b47bf5e7e0e268dddd65ddfb478fc96a8c7a
Zeph Levin <Zeph.levin@gmail.com>
2022-12-27 19:15:59 -0500

    Working notes, included htmlsanitizer package

--- src/t4mvc.web/Controllers/t4mvcApiController.cs
+++ src/t4mvc.web/Controllers/t4mvcApiController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using t4mvc.web.core.Models;
+using t4mvc.web.core.ViewModels;
 using t4mvc.web.core.ViewModelServices;
+using Ganss.Xss;
 
 namespace t4mvc.web.Controllers
 {
@@ -18,6 +20,19 @@ namespace t4mvc.web.Controllers
             var allUserList             = allUsers.ToList();
 
             return allUserList;
+        }
+
+        [Route("new-note")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public bool NewNote([FromForm] NoteViewModel noteViewModel)
+        {
+            var sanitizer           = new HtmlSanitizer();
+            noteViewModel.NoteText  = sanitizer.Sanitize(noteViewModel.NoteText);
+
+            noteViewModelService.CreateNote(noteViewModel);
+
+            return true;
         }
     }
 }
