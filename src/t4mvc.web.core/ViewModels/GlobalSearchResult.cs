e287884dde3efce62b28aecba66825e8f1f38531
Zeph Levin <Zeph.levin@gmail.com>
2022-12-28 07:20:11 -0500

    Created base for global search

---
+++ src/t4mvc.web.core/ViewModels/GlobalSearchResult.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t4mvc.web.core.ViewModels
+{
+    public class GlobalSearchResult
+    {
+        public List<SearchResultCategory> Categories { get; set; } = new List<SearchResultCategory>();
+    }
+
+    public class SearchResultCategory
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+        public List<SearchResultItem> Results { get; set; } = new List<SearchResultItem>();
+    }
+
+    public class SearchResultItem
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
